c9076da1120a4e10e73cc0d4471de833a634863f
tvparth <106056605+tvparth@users.noreply.github.com>
2022-11-14 18:28:26 +0530

    Update views.py

--- mail/views.py
+++ mail/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 def home(request):
     if request.method == 'POST':
@@ -7,5 +8,10 @@ def home(request):
         msg = request.POST.get('Message')
         email = request.POST.get('Email')
         print(sub,msg,email)
+        
+        send_mail(
+            sub,msg,'xyz@gmail.com',
+            [email]
+        )
         return HttpResponse("Email Send")
     return render(request,'home.html')
